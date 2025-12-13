0c347abb8bba051723ce4c34adba80bb12ea10f1
mkezlev <45949464+mkezlev@users.noreply.github.com>
2025-12-13 20:53:08 +0300

    Enhance location methods for randomness and uniqueness
    Added overloaded newLocation method to ensure new coordinates differ from given location. Updated getRandomNumber to include upper bound.

--- ProgJavaProject/src/main/java/game/Location.java
+++ ProgJavaProject/src/main/java/game/Location.java
@@ -26,6 +26,8 @@ public class Location {
         return this.y;
     }
 
+
+    // set new location x,y values
     public Location newLocation(int size){
         this.x = getRandomNumber(size);
         this.y = getRandomNumber(size);
@@ -33,9 +35,26 @@ public class Location {
 
     }
 
+   // set new location x,y values  
+   // location method overload to ensure new location is different than given location
+    public Location newLocation(int size, Location loc){
+        this.x = getRandomNumber(size);
+        int locx=loc.getX();
+        int locy=loc.getY();
+        while (this.x == locx){
+            this.x = getRandomNumber(size);
+        }
+        this.y = getRandomNumber(size);
+        while (this.y == locy){
+            this.y = getRandomNumber(size);
+        }
+        return this;
+    }
+
+    // generate random interger number in range 0-space
     private int getRandomNumber(int space) {
         Random rand = new Random();
-        int randomNumber = rand.nextInt(space);
+        int randomNumber = rand.nextInt(space+1);
         return randomNumber;
     }
 
